e54dfdab4fdd7cd727d700361740638177e9b12c
sebasso <sebass@student.matnat.uio.no>
2016-03-05 22:49:27 +0100

    filereader and gametest in unittest added

---
+++ 8.Hangman-Java/Hangman-Java/UnitTests/hangman1/FileReadWriterTest.java
@@ -0,0 +1,13 @@
+package hangman1;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+ * 1.This test only for filereadwriter
+ * 2. In integration testing test this with players.java and Game.java(not this file
+ */
+public class FileReadWriterTest {
+	
+}
---
+++ 8.Hangman-Java/Hangman-Java/UnitTests/hangman1/GameTest.java
@@ -0,0 +1,14 @@
+package hangman1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameTest {
+	
+	@Test
+	public void testGame(){
+		
+		
+	}
+	
+}
