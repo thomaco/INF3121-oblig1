931f10afa674bcfef6f3d3c4e50aea5f92439190
Thomas <thomaco@ulrik.uio.no>
2016-03-09 23:43:53 +0100

    Needs more comment, and lower avg depth if possible

--- 8.Hangman-Java/Hangman-Java/src/hangman/Game.java
+++ 8.Hangman-Java/Hangman-Java/src/hangman/Game.java
@@ -3,172 +3,146 @@ package hangman;
 import java.util.Random;
 import java.util.Scanner;
 
-// ve4e pi6a mnogo dobre na java, napisah vsi4ko za 2 4asa i trugna ot raz, yahuuaoaoaoaaauuuuu
 public class Game {
 	private static final String[] wordForGuesing = { "computer", "programmer",
 			"software", "debugger", "compiler", "developer", "algorithm",
 			"array", "method", "variable" };
 
-	private String guesWord;
-	
-	
-	
-	
-	private StringBuffer dashedWord;
+	private String guessWord;
 	private FileReadWriter filerw;
+    private boolean isHelpUsed;
+    private StringBuffer dashBuff;
 
-	public Game(boolean autoStart) {
-		guesWord = getRandWord();
-		dashedWord = getW(guesWord);
+    //Constructor
+	public Game() {
 		filerw = new FileReadWriter();
-		if(autoStart) {
-			displayMenu();
-		}
 	}
 
+    //Sets the variables and start the guess-loop
+	public void runHangman() {
+        StringBuilder dashedWord;
+        guessWord = wordForGuesing[new Random().nextInt(9)];
+        isHelpUsed = false;
+        dashedWord = new StringBuilder(new String(new char[guessWord.length()]).replace('\0', '_'));
+        dashBuff = new StringBuffer(dashedWord);
+		System.out.println("Welcome to �Hangman� game. Please, try to guess my secret word.\n"
+				+ "Use 'TOP' to view the top scoreboard, 'RESTART' to start a new game,"
+				+ "'HELP' to cheat and 'EXIT' to quit the game.");
 
-	private String getRandWord() {
-		Random rand = new Random();
-		String randWord = wordForGuesing[rand.nextInt(9)];
-		return randWord;
+		gameLoop();
 	}
-	public void displayMenu() {
-		System.out
-				.println("Welcome to �Hangman� game. Please, try to guess my secret word.\n"
-						+ "Use 'TOP' to view the top scoreboard, 'RESTART' to start a new game,"
-						+ "'HELP' to cheat and 'EXIT' to quit the game.");
 
-		findLetterAndPrintIt();
-	}
-	private void findLetterAndPrintIt() {
-		boolean isHelpUsed = false;
-		String letter = "";
-		StringBuffer dashBuff = new StringBuffer(dashedWord);
+    //Method containing the main game logic with loops and end-of-game message
+	private void gameLoop() {
+		String letter;
 		int mistakes = 0;
 
+        //Loops until word is complete
 		do {
-			System.out.println("The secret word is: " + printDashes(dashBuff));
-			System.out.println("DEBUG " + guesWord);
-			do {
-				System.out.println("Enter your gues(1 letter alowed): ");
-				Scanner input = new Scanner(System.in);
-				letter = input.next();
+            letter = inputLoop();
 
-				if (letter.equals(Command.help.toString())) {
-					isHelpUsed = true;
-					int i = 0, j = 0;
-					while (j < 1) {
-						if (dashBuff.charAt(i) == '_') {
-							dashBuff.setCharAt(i, guesWord.charAt(i));
-							++j;
-						}
-						++i;
-					}
-					System.out.println("The secret word is: "
-							+ printDashes(dashBuff));
-				}// end if
-				menu(letter);
-
-			} while (!letter.matches("[a-z]"));
-
-			int counter = 0;
-			for (int i = 0; i < guesWord.length(); i++) {
-				String currentLetter = Character.toString(guesWord.charAt(i));
-				if (letter.equals(currentLetter)) {
-					
-					{
-					
-					++counter;
-					}
-					dashBuff.setCharAt(i, letter.charAt(0));
-				}
-			}
+			int counter = replaceDashes(letter);
 
 			if (counter == 0) {
 				++mistakes;
-				{
-					System.out.printf(
-							"Sorry! There are no unrevealed letters \'%s\'. \n",
-							letter);
-				}
+				System.out.printf("Sorry! There are no unrevealed letters \'%s\'. \n", letter);
 			} else {
-				System.out.printf("Good job! You revealed %d letter(s).\n",
-						counter);
+				System.out.printf("Good job! You revealed %d letter(s).\n", counter);
 			}
 
-		} while (!dashBuff.toString().equals(guesWord));
+		} while (!dashBuff.toString().equals(guessWord));
 
-		if (isHelpUsed == false) {
-			System.out.println("You won with " + mistakes + " mistake(s).");
-			System.out.println("The secret word is: " + printDashes(dashBuff));
+        //Word complete, display end of game message
+        System.out.println("You won with " + mistakes + " mistake(s).");
+        System.out.println("The secret word is: " + printDashes(dashBuff) + ".");
+		if (!isHelpUsed) {
 
-			System.out
-					.println("Please enter your name for the top scoreboard:");
+			System.out.println("Please enter your name for the top scoreboard:");
 			Scanner input = new Scanner(System.in);
 			String playerName = input.next();
 
-			{
-				filerw.openFileToWite();
-				filerw.addRecords(mistakes, playerName);
-				filerw.closeFileFromWriting();
-				filerw.openFiletoRead();
-				filerw.readRecords();
-				filerw.closeFileFromReading();
-				filerw.printAndSortScoreBoard();
-			}
 
-			
+			filerw.openFileToWite();
+			filerw.addRecords(mistakes, playerName);
+			filerw.closeFileFromWriting();
+			sortAndPrintScore();
+
 		} else {
-			System.out
-					.println("You won with "
-							+ mistakes
-							+ " mistake(s). but you have cheated. You are not allowed to enter into the scoreboard.");
-			System.out.println("The secret word is: " + printDashes(dashBuff));
+			System.out.println("But since you cheated. You are not allowed to enter into the scoreboard.");
 		}
-		
+
 		// restart the game
-		new Game(true);
-		
-	}// end method
-	private void menu(String letter) {
-		if (letter.equals(Command.restart.toString())) {
-			new Game(true);
-		} else {
-			if (letter.equals(Command.top.toString())) {
-				filerw.openFiletoRead();
-				filerw.readRecords();
-				filerw.closeFileFromReading();
-				filerw.printAndSortScoreBoard();
-				new Game(true);
-			} else {
-				if (letter.equals(Command.exit.toString())) {
-					System.exit(1);
-				}
-			}
-		}
+        runHangman();
+
 	}
 
-	private StringBuffer getW(String word) {
-		StringBuffer dashes = new StringBuffer("");
-		for (int i = 0; i < word.length(); i++) {
-			dashes.append("_");
-			
-			
-			
+    //Loop which handles input, returns a letter
+    private String inputLoop() {
+        String letter;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("The secret word is: " + printDashes(dashBuff));
+        System.out.println("DEBUG " + guessWord);
+
+        do {
+            System.out.println("Enter your guess(1 letter allowed): ");
+            letter = input.next();
+
+            if(dashBuff.indexOf(letter) > -1 && letter.length() < 2) {
+                letter = "-";
+                System.out.println("Letter already guessed.");
+            }
+            letter = commandCheckAndExecute(letter);
+        } while (!letter.matches("[a-z]"));
+
+        return letter;
+    }
+
+    //Check if String word is a command, and executes action if it is
+	private String commandCheckAndExecute(String word) {
+        if(word.equals(Command.help.toString())) {
+            isHelpUsed = true;
+            int i = dashBuff.indexOf("_");
+            word = String.valueOf(guessWord.charAt(i));
+        } else if (word.equals(Command.restart.toString())) {
+            runHangman();
+		} else if (word.equals(Command.top.toString())) {
+            sortAndPrintScore();
+            runHangman();
+		} else if (word.equals(Command.exit.toString())) {
+			System.exit(1);
 		}
-		return dashes;
+        return word;
 	}
+
+    //Wrapper for turning a StringBuffer word into a space-separated
 	private String printDashes(StringBuffer word) {
 		String toDashes = "";
-		
-		
 		for (int i = 0; i < word.length(); i++) {
-			
-			
-			
 			toDashes += (" " + word.charAt(i));
 		}
 		return toDashes;
 
 	}
+
+    //Wrapper for reading and printing scores
+    private void sortAndPrintScore() {
+        filerw.openFiletoRead();
+        filerw.readRecords();
+        filerw.closeFileFromReading();
+        filerw.printAndSortScoreBoard();
+    }
+
+    //Wrapper which replaces underscores in dashBuff if the correct letter is guessed
+    private int replaceDashes(String word) {
+        int counter = 0;
+        for (int i = 0; i < guessWord.length(); i++) {
+            String currentLetter = Character.toString(guessWord.charAt(i));
+            if (word.equals(currentLetter)) {
+                ++counter;
+                dashBuff.setCharAt(i, word.charAt(0));
+            }
+        }
+        return counter;
+    }
 }
